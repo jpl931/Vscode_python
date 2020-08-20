9b968818adbeb6217e386dad9a5bbf08622f2aad
jpl931 <jpl931@gmail.com>
2020-08-20 15:27:36 -0500

    changed guesses to 5 times from 6

--- guessTheNumber.py
+++ guessTheNumber.py
@@ -4,9 +4,9 @@ secretNumber = random.randint(1, 20)
 
 print('I am thinking of a number between 1 and 20')
 
-#Ask the the player to guess 6 times.
+#Ask the the player to guess 5 times.
 
-for guessesTaken in range(1, 7):
+for guessesTaken in range(1, 6):
     print('Take a guess. ')
     guess = int(input())
 
