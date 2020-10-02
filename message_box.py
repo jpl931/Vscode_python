91bcdf716ace722136cde39afd36aabd8d672657
jpl931 <jpl931@gmail.com>
2020-10-02 12:44:17 -0500

    Line 6

--- message_box.py
+++ message_box.py
@@ -3,4 +3,4 @@ from tkinter import messagebox
 
 root = tk.Tk()
 root.withdraw()
-messagebox.showwarning('End of Data', "You have reached the end of the data. Please turn around!")
+messagebox.showwarning('End of Data', "You have reached the end of the internet. Please turn around!")
