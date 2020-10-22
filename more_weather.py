9bdafe2b8f6bec9f388051ebce4028086c747ba4
jpl931 <jpl931@gmail.com>
2020-10-22 16:07:56 -0500

    test 1

---
+++ more_weather.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+api_key = "518024a766d32327acf4b49f5e47e52f"
+
+#where"
+
+lat = "38.627003"
+
+lon = "-90.199402"
+
+url ="https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&units=metric" % (lat, lon, api_key)
+
+
+response =requests.get(url)
+
+data =json.loads(response.text)
+
+print(data)
