e16d3bb1b60dae854fc561b841ba46aef09345b1
jpl931 <jpl931@gmail.com>
2020-12-14 15:13:00 -0600

    cleanup

--- owm.py
+++ owm.py
@@ -7,10 +7,10 @@ from pyowm.utils import timestamps
 owm = OWM('518024a766d32327acf4b49f5e47e52f')
 mgr = owm.weather_manager()
 
-#---------------------current weather Lond,GB
+#---------------------current weather London,GB
 
-w = observation.weather
+#w = observation.weather
 
 
-#Foreczst Lomdon
+#Forecast Lomdon
 forecast = mgr.forecast_at_place('London,GB', 'daily')
