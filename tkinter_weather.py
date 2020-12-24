5e5963ee0af6f792522b50c7f441bd7a086b6e58
jpl931 <jpl931@gmail.com>
2020-12-24 17:35:00 -0600

    Still broken

--- tkinter_weather.py
+++ tkinter_weather.py
@@ -3,15 +3,15 @@ import pyowm
 
 def open_weather_map(event):
     owm = pyowm.OWM('518024a766d32327acf4b49f5e47e52f')
-    city_name = entry.get()
-    observation = owm.weather_at_place
+    city_name = entry1.get()
+    observation = owm.weather_at_place('Los Angeles, US')
     w = observation.get_weather()
     temp = w.get_temperature(unit = 'imperial')
     label1['temp'] = temp
 
 window = Tk()
 window.geometry('400x500')
-window.title('Weather)')
+window.title('Weather')
 
 entry1 = Entry(window, width = 25, font = ('Arial' ,16))
 entry1.pack()
@@ -22,8 +22,7 @@ button1.pack()
 label1 = Label(window, font = ('Arial' , 20, 'bold'))
 label1.pack()
 
-button1 = Button(window, text = 'Submit'), font = ('Arial', 16), command=open_weather_map)
-window.main.loop()
-
+button1.bind("<Button-1>", open_weather_map)
+window.mainloop()
 
 
