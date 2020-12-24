c841fd7284a7bc452c14f4ed5963365a8d4777c2
jpl931 <jpl931@gmail.com>
2020-12-24 16:47:01 -0600

    line 2 = sign added

--- tkinter_weather.py
+++ tkinter_weather.py
@@ -2,11 +2,10 @@ from tkinter import *
 import pyowm
 
 def open_weather_map():
-    owm pyowm.OWM('518024a766d32327acf4b49f5e47e52f')
+    owm = pyowm.OWM('518024a766d32327acf4b49f5e47e52f')
     city_name = entry.get()
     observation = owm,weather_at_place
     w = observation.get_weather()
     temp = w.get_temperature(unit = 'imperial')
     label1['temp'] = temp
 
-    
