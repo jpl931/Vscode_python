b7bbf175eee6f6478b8e0af6087b20512119a25d
jpl931 <jpl931@gmail.com>
2021-01-03 17:22:26 -0600

    ver 1.0 line 21

---
+++ 5_day_weather_forecast.py
@@ -0,0 +1,21 @@
+import requests
+import calendar
+
+
+api_key = '518024a766d32327acf4b49f5e47e52f'
+api_call = 'https;//api.openweathermap.org/data/2.5/forecast?appid=' +api_key
+
+running = True
+
+print('Welcome to John\'s 5 day weather forecast app using Open Weather Map\'s API!')
+
+#program loop
+while running:
+
+    while True:
+
+        # Input validation
+        try:
+            print('\nThis app supports search by city(0) or by zip code(1).')
+            search = int(input('Please input 0 or 1: '))
+            
