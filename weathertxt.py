04a814ded497562371659e9d2a9ed1a2ea03bc33
jpl931 <jpl931@gmail.com>
2021-01-03 17:08:02 -0600

    saved something don't know what

--- weathertxt.py
+++ weathertxt.py
@@ -4,8 +4,8 @@ from datetime import date
 
 def get_weather(city, units):
     today = date.today()
-    API_KEY = "api_address = 'http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&APPID=518024a766d32327acf4b49f5e47e52f&q='
-"
+    API_KEY = "api_address = 'http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&APPID=518024a766d32327acf4b49f5e47e52f&q="
+
     URL =f'https://api.openweathermap.org/data/2.5/weather?zip=63048,us&appid={API_KEY}&units={units}'
 
 
