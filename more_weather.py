df0fa1af9c572c1f0fc7121dddbe6d97768338aa
jpl931 <jpl931@gmail.com>
2021-01-15 19:28:00 -0600

    line 23

--- more_weather.py
+++ more_weather.py
@@ -20,7 +20,7 @@ response = requests.get(url)
 data = json.loads(response.text)
 
 print(data)
-pp = pprint.PrettyPrinter(indent=7)
+pp = pprint.pprint(indent=7)
 pprint(response.json())
 
 #location = data['location']['name']
