9dd688495f9bd35550a3ecde8bb2fec4e5a7f853
jpl931 <jpl931@gmail.com>
2021-01-19 16:12:11 -0600

    part 2

--- pygame_test1.py
+++ pygame_test1.py
@@ -23,3 +23,21 @@ SCREEN_HEIGHT = 600
 # create screen object
 # the size by constant screen_width, screen_height
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+# variable to keep the main loop runnning
+running = True
+
+# main loop
+while running:
+    # look at every event in the queue
+    for event in pygame.event.get():
+        # did the user hit a key?
+        if event.type == KEYDOWN:
+            # was it the escape key? If so, stop the loop.
+            if event.key == K_ESCAPE:
+                running = False
+
+        # did the user click the close button? Then stop the loop.
+        elif event.type == QUIT:
+            running = False
+            
