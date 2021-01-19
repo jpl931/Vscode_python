c4302880e34a384cc6dbf974afabfffaef06ece6
jpl931 <jpl931@gmail.com>
2021-01-18 20:09:41 -0600

    pylint doesnt like what I did

---
+++ pygame_test.py
@@ -0,0 +1,29 @@
+# Simple pygame stuff
+
+# Import and init the pygame library
+import pygame
+pygame.init()
+
+# Setup the drawing window
+screen = pygame.display.set_mode([500, 500])
+
+# Run until the user asks to quit
+running = True
+while running:
+
+    # Did the user click the window close button?
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Fill the background to white
+    screen.fill((255, 255, 255))
+
+    #Draw a solid blue circle in the center
+    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+
+    # Flip the display
+    pygame.display.flip()
+
+# Done! Time to quit
+pygame.quit()
