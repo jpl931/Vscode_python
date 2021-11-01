edfd1de2e25f8ecbe331aa0067220f1e776424bb
jpl931 <jpl931@gmail.com>
2021-11-01 13:13:30 -0500

    express time server?

--- test.js
+++ test.js
@@ -1,9 +1,1 @@
-function calculateDaysBetweenDates(begin, end) {
-    var startDay = new Date(begin);
-    var endDay = new Date(end);
-    var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
-    var diffDays = Math.round(Math.abs((startDay.getTime() - endDay.getTime())/(oneDay)));
-    return diffDays;
-}{
-    
-}
+// Express server on port 3000  
