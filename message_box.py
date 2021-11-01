7cfbda0e2f79cf966c482ace21de1b92774a0b9c
jpl931 <jpl931@gmail.com>
2021-11-01 11:52:53 -0500

    added more text

--- message_box.py
+++ message_box.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox
+#Test of GitHub Copilpilot
 
 root = tk.Tk()
 root.withdraw()
-messagebox.showwarning('End of Data', "You have reached the end of the internet. Please turn around!")
+messagebox.showwarning('End of Data', "You have reached the end of the internet. Please turn around right now!")
