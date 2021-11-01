01dca64d9d432f6e526648c6b0903962b4dbc7e9
jpl931 <jpl931@gmail.com>
2021-11-01 12:09:12 -0500

    remove space line 1

--- calculate.js
+++ calculate.js
@@ -1,4 +1,4 @@
-function calculateDays BetweenDates(begin, end) {
+function calculateDaysBetweenDates(begin, end) {
   var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
   var firstDate = new Date(begin);
   var secondDate = new Date(end);
