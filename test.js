fce8c490595fa61989801b3b4f8fe9718091c5ba
jpl931 <jpl931@gmail.com>
2021-11-01 12:33:24 -0500

    some more stuff

--- test.js
+++ test.js
@@ -1,3 +1,9 @@
 function calculateDaysBetweenDates(begin, end) {
+    var startDay = new Date(begin);
+    var endDay = new Date(end);
+    var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
+    var diffDays = Math.round(Math.abs((startDay.getTime() - endDay.getTime())/(oneDay)));
+    return diffDays;
+}{
     
 }
