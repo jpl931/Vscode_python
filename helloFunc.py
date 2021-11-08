ceabbddf925dcc391a11d9ae2f5ffe475274ced2
jpl931 <jpl931@gmail.com>
2021-11-08 12:35:12 -0600

    time between dates

--- helloFunc.py
+++ helloFunc.py
@@ -1,15 +1,21 @@
-def hello():
-    print('Howdy!')
-    print('Howdy!!')
-    print('Hello There.')
-hello()
-hello()
-hello()
-a = 2 * 2
-b = 3 * 3
-print (a)
-print (b)
-print (a + b)
-print(a + a + b + b)
-print(a + a + a + a + b + b + b + b) #copilot suggested the reamining entries
-print(a + a + a + a + a + a + a + a + b + b + b + b + b + b + b + b + b)
+'time between dates'
+def date():
+    import datetime
+    now = datetime.datetime.now()
+    print(now)
+    print(now.year)
+    print(now.month)
+    print(now.day)
+    print(now.hour)
+    print(now.minute)
+    print(now.second)
+    print(now.microsecond)
+    print(now.weekday())
+    days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    print(days[now.weekday()])
+    print(now.strftime('%Y-%m-%d %H:%M:%S'))
+    print(now.strftime('%A, %B, %d, %Y'))
+
+
+
+
