5601063bd4444a361eb80606c0c72ec0fec521c4
jpl931 <jpl931@gmail.com>
2022-01-04 16:33:29 -0600

    more updates

--- pizza_order.py
+++ pizza_order.py
@@ -15,4 +15,20 @@ for k, v in myMenu.items():
     print(i, k, v)
     i += 1
 
+ itemNumber = int(input("Please enter the number of the item you would like to order: "))
+ item=list(myMenu.keys())[itemNumber-1]
+
+ print("You have ordered:", item)
+ print("The price is:", myMenu[item])
+ print("Thank you for your order!")
+ 
+ # print(myMenu))
+ # print(myMenu.keys())
+ # print(myMenu.values())
+ # print(myMenu.items())
+    # print(myMenu.get("Sandwich"))
+    # print(myMenu.get("Burger"))
+    # print(myMenu.get("Steak"))
+    # print(myMenu.get("Pizza"))
+    
     
