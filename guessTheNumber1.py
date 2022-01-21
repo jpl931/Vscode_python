13ac8b4b0a21297a4e8069d3f8899cacd24d3742
jpl931 <jpl931@gmail.com>
2022-01-21 14:27:15 -0600

    new 1/24/21

---
+++ guessTheNumber1.py
@@ -0,0 +1,13 @@
+#Python guessing name game
+
+from random import random, randint
+import random
+
+
+#Number of tries 10
+tries = 10
+
+#Title of the game
+
+print("===Guess the Number===")
+print(f"You have {tries} tries to guess the number between 1 and 10")
