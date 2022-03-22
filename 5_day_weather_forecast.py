ffab7dfcceefd4a1f066d65863e16ef68c02b3eb
jpl931 <jpl931@gmail.com>
2022-03-21 20:48:50 -0500

    didnt need it

--- 5_day_weather_forecast.py
+++ 5_day_weather_forecast.py
@@ -3,7 +3,7 @@ import calendar
 
 
 api_key = '518024a766d32327acf4b49f5e47e52f'
-api_call = 'https://api.openweathermap.org/data/2.5/forecast?appid=' + api_key + units=imperial
+api_call = 'https://api.openweathermap.org/data/2.5/forecast?appid=' + api_key 
 
 running = True
 
