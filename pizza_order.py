017b5e7bf80a4c78c1a6281d79dc8b77498e6fda
jpl931 <jpl931@gmail.com>
2022-03-29 17:04:54 -0500

    pizza order 1.0

---
+++ pizza_order.py
@@ -0,0 +1,23 @@
+#Pizza ordering system
+import os
+available_pizzas = ['Margherita', 'Pepperoni', 'Hawaiian', 'Vegetarian']
+available_toppings = ['Cheese', 'Pepperoni', 'Sausage', 'Mushrooms', 'Onions', 'Olives', 'Pineapple']   
+pizza_prices = {'Margherita': 10, 'Pepperoni': 12, 'Hawaiian': 14, 'Vegetarian': 8}
+pizza_toppings = {'Margherita': ['Cheese'], 'Pepperoni': ['Cheese', 'Pepperoni'], 'Hawaiian': ['Cheese', 'Ham', 'Pineapple'], 'Vegetarian': ['Cheese', 'Mushrooms', 'Onions']}
+
+def ShowMenu():
+    os.system('cls')
+    print("Available pizzas:\n")
+    print(*available_pizzas, sep =' ,  ')
+    print("\nAvailable toppings:\n")
+    print(*available_toppings, sep =' ,  ')
+    print("\n\n")   
+
+def TakeOrderInput():
+    order = input("Please enter your order: ")
+    ordering = True
+    while ordering:
+        os.system('cls')
+        ShowMenu()
+        pizza = input()
+    
