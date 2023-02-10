e5f7c0a8664d026dcd2596604fc0d1f6597262d7
jpl931 <jpl931@gmail.com>
2023-02-10 12:09:19 -0600

    powerball.py ver 0.1

---
+++ powerball.py
@@ -0,0 +1,20 @@
+import random
+
+tickets_per_drawing = 1
+num_drawings = 1
+
+total_spent = 0
+earnings = 0
+
+times_won = {
+    "5+P": 0,
+    "5": 0,
+    "4+P": 0,
+    "4": 0,
+    "3+P": 0,
+    "3": 0,
+    "2+P": 0,
+    "1+P": 0,
+    "P": 0,
+    "0": 0.
+}
