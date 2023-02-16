233daa249d6cdba155cdb79e3d703e1c5bf41609
jpl931 <jpl931@gmail.com>
2023-02-16 16:08:02 -0600

    Updating code per video

--- powerball.py
+++ powerball.py
@@ -18,3 +18,23 @@ times_won = {
     "P": 0,
     "0": 0.
 }
+
+def calc_win_amt(my_numbers, winning_numbers):
+    win_amt = 0
+
+
+for drawing in range(num_drawings):
+    white_drawing = set(random.sample(white_possibles, k=5))
+    red_drawing = random.choice(red_possibles)
+
+    winning_numbers = {"whites": white_drawing, "red": red_drawing}
+
+    for ticket in range(tickets_per_drawing):
+        total_spent += 2
+        my_whites = set(random.sample(white_possibles, k=5))
+        my_red = random.choice(red_possibles)
+
+
+        myNumbers = {"whites": my_whites, "red": my_red}
+
+        #calc_win_amt
