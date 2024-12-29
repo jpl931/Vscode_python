da8fece93042c27be7bd076f72ada79ce37c6910
jpl931 <jpl931@gmail.com>
2024-12-28 21:12:13 -0600

    deleted file

--- message_box.py
+++
@@ -1,7 +0,0 @@
-import tkinter as tk
-from tkinter import messagebox
-#Test of GitHub Co-pilot tada...
-
-root = tk.Tk()
-root.withdraw()
-messagebox.showwarning('End of Data', "You have reached the end of the internet. Please turn around IMMEDIATELY!")
