9b30ea7c7290f36f24ea1a98bcb56d1b891b0768
jpl931 <jpl931@gmail.com>
2025-02-27 13:24:10 -0600

    trying to chnge this stuff

--- pwordcracker.py
+++ pwordcracker.py
@@ -1,6 +1,6 @@
 import itertools
 
-def brute_force_crack(password, charset='abcdefghijklmnopqrstuvwxyz'):
+def brute_force_crack(password, charset='abcdefghijklmnopqrstuvwxyz):
     for length in range(1, 9):  # Adjust the range for longer passwords
         for attempt in itertools.product(charset, repeat=length):
             attempt = ''.join(attempt)
